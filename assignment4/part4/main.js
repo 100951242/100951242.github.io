6682b3f0ae9b1161d6a41ed5401a95cb1c87eee7
100951242 <anjal.kafle@dcmail.ca>
2024-08-05 00:53:18 -0400

    updating the code

--- assignment4/part4/main.js
+++ assignment4/part4/main.js
@@ -119,7 +119,25 @@ class EvilCircle extends shape {
     ctx.stroke();
   }
 
+  // checking bonds
+  
+  checkBounds() {
+    if ((this.x + this.size) >= width) {
+      this.x -= this.size;
+    }
 
+    if ((this.x - this.size) <= 0) {
+      this.x += this.size;
+    }
+
+    if ((this.y + this.size) >= height) {
+      this.y -= this.size;
+    }
+
+    if ((this.y - this.size) <= 0) {
+      this.y += this.size;
+    }
+  }
 
 const balls = [];
 
